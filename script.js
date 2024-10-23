d78891dc17f9cfe047f206c50dbfb4fa39aa96bc
Luca Lamorte <86692563+lucalamorte@users.noreply.github.com>
2024-10-23 02:15:33 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
+// Función para obtener los parámetros de la URL
+function getQueryParam(param) {
+    const urlParams = new URLSearchParams(window.location.search);
+    return urlParams.get(param);
+}
+
+// Verificar si la URL contiene el parámetro 'query'
+function checkAndSetQuery() {
+    const queryParam = getQueryParam('query');
+    
+    // Si 'query' no está presente, agregarlo a la URL
+    if (queryParam === null) {
+        // Actualizar la URL sin recargar la página
+        window.history.replaceState({}, document.title, "?query=");
+    }
+
+    // Si 'query' existe (aunque esté vacío), podemos rellenar el campo de búsqueda
+    document.getElementById('searchInput').value = queryParam || '';
+}
+checkAndSetQuery();
+
 // Seleccionar los elementos del DOM
 const searchInput = document.getElementById('searchInput');
 const searchButton = document.getElementById('searchButton');
@@ -5,7 +26,6 @@ const searchHistory = document.getElementById('searchHistory');
 
 // Cargar historial de búsquedas de localStorage al cargar la página
 document.addEventListener('DOMContentLoaded', loadSearchHistory);
-
 // Agregar la búsqueda al hacer clic en el botón
 searchButton.addEventListener('click', addSearch);
 
